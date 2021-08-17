13bf477ae6239292197ee4e46c5a8194a5f66815
progmae20 <69871765+progmae20@users.noreply.github.com>
2021-08-17 12:57:23 +0300

    Project created
    Commit 1

---
+++ HomeWork13/HomeWork13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork13
+//
+//  Created by Anastasiya on 17.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
